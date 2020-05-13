2de8ff8cd88b1b73c8dc35e2550dc0516be08710
GiadaD94 <62468204+GiadaD94@users.noreply.github.com>
2020-05-13 14:32:00 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -1,34 +1,34 @@
-#Primo codice R
+# Primo codice R
 
 # installazione pacchetti, uso le virgolette per prendere qualcosa che è al di fuori di R, le citazioni successive senza virgolette
 install.packages("sp")
 
 library(sp)
 
-#inserisco i dati di un data set, in questo caso meuse
+# inserisco i dati di un data set, in questo caso meuse
 data(meuse)
 
 meuse
 
-# head è funzione per inserire solo la parte iniziale di un data set, la testa
+# head è funzione per inserire solo la parte iniziale di un data set, la testata
 head(meuse)
 
-#nomi delle variabili del data set
+# nomi delle variabili del data set
 names(meuse)
 
-#riassunto
+# riassunto
 summary(meuse)
 
 
 
-#plot è funzione per mettere in relazione delle variabili, all'interno di meuse in questo caso e si esprime con il dollaro $
+# plot è funzione per mettere in relazione delle variabili (all'interno di meuse in questo caso) e si esprime con il dollaro $
 plot(meuse$cadmium,meuse$lead)
 
-#allegare dataframe permette di non usare piu meuse$
+# allegare dataframe permette di non usare piu meuse$
 attach(meuse)
 plot(cadmium,lead)
 
-#grafico migliorato (pch : da cercare su google per prendere i caratteri, cex sta per character exageration, quindi aumento dimensioni del carattere, mentre col sta per colore)
+# grafico migliorato (pch : da cercare su google per prendere i caratteri, cex sta per character exageration, quindi aumento dimensioni del carattere, mentre col sta per colore)
 plot(cadmium,lead,pch=17,cex=3,col="yellow")
 
 # pairs è funzione che comprende tanti plot ovvero fra tutte le variabili possibili del data set, si apre un subset con la quadra seguita dalla virgola, i due punti indicano fino a che punto si arriva. La tilde si esegue con Alt+126
@@ -36,5 +36,5 @@ pairs(~cadmium+copper+lead,data=meuse)
 pairs(meuse[,3:6])
 pairs(meuse[,3:6],col="green")
 
-#main"nomegrafico"
+# main"nomegrafico"
 main=primo pairs
